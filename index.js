7ae6732e3d6aa74062866e09706e873aeaa35e23
aartibhutra <aartimaheshwari160@gmail.com>
2025-04-18 06:09:10 +0530

    mongoose

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const userRoute = require('./routes/user');
 
 const app = express();
 const PORT = 8000;
+mongoose.connect("mongodb://localhost:27017/Blogora").then(e => console.log("MongoDB connected")).catch(e => console.log(e));
+
+// form data parsing middleware
+app.use(express.urlencoded({extended:false}));
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve("./views"));
