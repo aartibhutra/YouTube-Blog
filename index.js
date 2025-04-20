e988360d46ac1b30e2847a38ced72decc858a205
aartibhutra <aartimaheshwari160@gmail.com>
2025-04-20 05:51:19 +0530

    cookie-Parser

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 
 const userRoute = require('./routes/user');
+const { checkForAuthenticationCookie } = require('./middlewares/authentication');
 
 const app = express();
 const PORT = 8000;
@@ -10,12 +12,16 @@ mongoose.connect("mongodb://localhost:27017/Blogora").then(e => console.log("Mon
 
 // form data parsing middleware
 app.use(express.urlencoded({extended:false}));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
 
 app.set('view engine', 'ejs');
 app.set('views', path.resolve("./views"));
 
 app.get('/' , (req , res) => {
-    res.render("home");
+    res.render("home" , {
+        user: req.user,
+    });
 });
 
 app.use('/user', userRoute);
