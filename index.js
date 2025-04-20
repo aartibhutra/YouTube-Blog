e6087a0a278b143e8ae14e7d68b9d66c24921fda
aartibhutra <aartimaheshwari160@gmail.com>
2025-04-20 07:01:14 +0530

    blog changes

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
 const userRoute = require('./routes/user');
+const blogRoute = require('./routes/blog');
 const { checkForAuthenticationCookie } = require('./middlewares/authentication');
 
 const app = express();
@@ -25,5 +26,6 @@ app.get('/' , (req , res) => {
 });
 
 app.use('/user', userRoute);
+app.use('/blog', blogRoute);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
