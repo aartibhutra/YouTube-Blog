ade874f7fa5be085fecab6b6b49e60b14d94ff01
aartibhutra <aartimaheshwari160@gmail.com>
2025-04-26 10:47:04 +0530

    DiskStorage

--- routes/blog.js
+++ routes/blog.js
@@ -1,6 +1,22 @@
 const {Router} = require('express');
+const multer = require('multer');
+const path = require('path');
 
 const router = Router();
+
+//DiskStorage for Multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.resolve(`./public/uploads/${req.user._id}`));
+    },
+    filename: function (req, file, cb) {
+      const fileName = `${Date.now()}-${file.originalname}`
+      // call callback
+      cb(null , fileName);
+    },
+  });
+  
+const upload = multer({ storage: storage })
 
 router.get("/add-new" , (req , res) => {
     return res.render("addBlog",{
@@ -8,7 +24,7 @@ router.get("/add-new" , (req , res) => {
     });
 });
 
-router.post("/" , (req , res) => {
+router.post("/" ,upload.single('coverImage'), (req , res) => {
     return res.redirect("/");
 });
 
